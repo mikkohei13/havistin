59a0cf96ebfa7e571430c2e7191fbe756dfa7b43
Mikko Heikkinen <hawk@biomi.org>
2016-08-26 21:50:27 +0300

    API uses preset id, if exists

--- api/index.php
+++ api/index.php
@@ -9,11 +9,17 @@ foreach ($_POST as $key => $value)
 }
 
 // Todo: Take the object id from post, if exists
+if (isset($dataDirty['id']))
+{
+	$uriId = $dataDirty['id'];
+}
+else
+{
+	$objectId = "HAVISTIN-" . rand(100000, 999999);
+	$uriId = "http://tun.fi/JA." . $objectId; // dummy
+}
 
-$objectId = "HAVISTIN-" . rand(100000, 999999);
-$uriId = "http://tun.fi/JA." . $objectId; // dummy
-
-$filename = "data/" . $objectId . ".json";
+$filename = "data/" . sha1($uriId) . ".json";
 
 file_put_contents($filename, json_encode($dataDirty));
 
